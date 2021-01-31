fec2f8067c5b7a5e9d7f9f9ccec15d53758a3560
Alexander Butenko <a.butenka@gmail.com>
2021-01-31 18:42:10 -0400

    remove cJSON

--- install.sh
+++ install.sh
@@ -3,7 +3,4 @@ if [ ! -s include/asterisk.h ] ; then
 	exit
 fi
 cp asterisk-res_json/res_json.c addons/.
-cp asterisk-res_json/cJSON.h include/asterisk/.
-cp asterisk-res_json/cJSON.c addons/.
-echo "edit addons/Makefile: add res_json to the list of modules built, and"
-echo "    res_json.so: cJSON.o res_json.o"
+echo "edit addons/Makefile: add res_json to the list of modules built"
