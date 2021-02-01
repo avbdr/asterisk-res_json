048b5cd13b755aab041096304548d4e65e3df3df
Alexander Butenko <a.butenka@gmail.com>
2021-02-01 00:19:46 -0400

    install.sh cleanup

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@ if [ ! -s include/asterisk.h ] ; then
 	echo "please cd into the directory where the asterisk source has been untarred"
 	exit
 fi
-cp asterisk-res_json/res_json.c addons/.
+cp asterisk-res_json/res_json.c addons/
 echo "edit addons/Makefile: add res_json to the list of modules built"
